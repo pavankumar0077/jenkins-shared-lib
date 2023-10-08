d71085a76f18a6fd6dd545855f66d0fcc1347c29
pavankumar0077 <dasarepavan007@gmail.com>
2023-10-08 21:12:13 +0530

    with cred docker

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,9 @@
-def call(String project,String ImageTag, String hubUser){
+def call(credentialsId, String project,String ImageTag, String hubUser){
+    withDockerRegistry(credentialsId: credentialsId) {
     sh """
      sudo docker image build -t ${hubUser}:${project} .
      sudo docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
      sudo docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
     """
+    }
 }
